8b03ebe3a02d173d49a31a73ecaffd9fa0608145
Vijay <74645268+vijaysingh2219@users.noreply.github.com>
2025-09-16 17:33:59 +0530

    feat: design home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,40 @@
+'use client';
+
+import Logo from '@/components/logo';
 import { Button } from '@workspace/ui/components/button';
+import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+    <main className="m-auto flex items-center justify-center px-4">
+      <div className="bg-background/70 flex max-w-2xl flex-col items-center gap-5 rounded-xl p-8 text-center backdrop-blur">
+        <Logo variant="default" />
+        <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
+          Turborepo Starter Template
+        </h1>
+        <span className="bg-muted text-muted-foreground rounded px-2 py-1 text-xs font-medium">
+          Fullstack • Monorepo • TypeScript • Next.js • shadcn/ui
+        </span>
+        <p className="text-muted-foreground max-w-md">
+          A starter template for building fullstack applications with Next.js and Turborepo.
+          Includes authentication, a responsive UI, and a modern development setup.
+        </p>
+        <div className="mt-4 flex flex-col gap-3 sm:flex-row">
+          <Button asChild size="sm">
+            <Link href="https://github.com/vijaysingh2219/turborepo-starter" target="_blank">
+              View on GitHub
+            </Link>
+          </Button>
+          <Button asChild variant="secondary" size="sm">
+            <Link
+              href="https://github.com/vijaysingh2219/turborepo-starter?tab=readme-ov-file#readme"
+              target="_blank"
+            >
+              Read Docs
+            </Link>
+          </Button>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
