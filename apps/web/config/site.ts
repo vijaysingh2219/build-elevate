12ca6b9cd98bb5cdc9ec226fb4274114234d0c6b
Vijay <74645268+vijaysingh2219@users.noreply.github.com>
2026-03-05 09:39:39 +0530

    feat(web): add metadataBase to site config

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -9,6 +9,7 @@ export const siteConfig: Metadata = {
     apple: '/apple-touch-icon.png',
     shortcut: '/favicon.ico',
   },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? 'https://nexa-web-app.vercel.app'),
   keywords: [
     'turborepo',
     'starter',
